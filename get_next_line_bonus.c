8693116010fd3096659f660f7de88006b6ca5261
MerleneShushi <117476220+MerleneShushi@users.noreply.github.com>
2022-11-30 14:15:23 +0000

    Add files via upload

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 static char	*ft_strchr(const char *str, int b)
 {
--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
